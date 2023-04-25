861d724597eb894df485cc69fc01e957a9b95ec5
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2023-04-25 09:37:38 +0200

    init

---
+++ src/main/java/dk/kea/unittestdemo/service/Calculator.java
@@ -0,0 +1,9 @@
+package dk.kea.unittestdemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+
+
+}
---
+++ src/test/java/dk/kea/unittestdemo/UnitTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.unittestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
